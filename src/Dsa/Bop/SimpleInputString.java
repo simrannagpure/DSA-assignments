fd674b0edbd26d287eb70dca77ed0170e2fc79d8
Simran Nagpure <simrannagpure96@gmail.com>
2023-06-11 20:27:09 +0530

    BOP assignments

---
+++ src/Dsa/Bop/SimpleInputString.java
@@ -0,0 +1,12 @@
+package Dsa.Bop;
+
+public class SimpleInputString {
+    public static void main(String args[]){
+        printString("hello guys");
+    }
+    static void printString(String stringVariable){
+        /* Write your code here*/
+        String S = stringVariable;
+        System.out.println(S);
+    }
+}
